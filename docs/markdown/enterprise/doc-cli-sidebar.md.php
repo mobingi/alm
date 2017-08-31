9dc8dfb0fefba2dab9889a4dea5140ac44debb5f
flowerinthenight <chew.esmero@gmail.com>
2017-08-31 11:05:06 +0900

    Update cli sidebar.

--- docs/markdown/enterprise/doc-cli-sidebar.md.php
+++ docs/markdown/enterprise/doc-cli-sidebar.md.php
@@ -8,6 +8,39 @@
             <li><a href="#stack-list">list</a></li>
             <li><a href="#stack-describe">describe</a></li>
             <li><a href="#stack-create">create</a></li>
+            <li><a href="#stack-update">update</a></li>
+            <li><a href="#stack-delete">delete</a></li>
         </ul>
     </li>
+    <li>
+        <a href="#template-versions">template</a>
+        <ul>
+            <li><a href="#template-versions">versions</a></li>
+            <li><a href="#template-compare">compare</a></li>
+        </ul>
+    </li>
+    <li>
+        <a href="#svrconf-show">[v2] svrconf</a>
+        <ul>
+            <li><a href="#svrconf-show">show</a></li>
+            <li><a href="#svrconf-update">update</a></li>
+        </ul>
+    </li>
+    <li>
+        <a href="#creds-list">creds</a>
+        <ul>
+            <li><a href="#creds-list">list</a></li>
+        </ul>
+    </li>
+    <li>
+        <a href="#registry-list-catalog">registry</a>
+        <ul>
+            <li><a href="#registry-list-catalog">catalog</a></li>
+            <li><a href="#registry-list-tags">tag</a></li>
+            <li><a href="#registry-tag-manifest">manifest</a></li>
+            <li><a href="#registry-tag-delete">delete</a></li>
+            <li><a href="#registry-get-token">token</a></li>
+        </ul>
+    </li>
+    <li><a href="#version">version</a></li>
 </ul>
