119bdb1320bd3d9b093b477ba2224b0829a23888
flowerinthenight <chew.esmero@mobingi.com>
2017-08-31 10:39:09 +0900

    Add sidebar to cli docs.

---
+++ docs/markdown/enterprise/doc-cli-sidebar.md.php
@@ -0,0 +1,13 @@
+<!-- Sidebar is not parsed thru script to give you more control over style-->
+
+<ul class="nav sidenav dropable sticky">
+    <li class="active"><a href="#login">login</a></li>
+    <li>
+        <a href="#stack-list">stack</a>
+        <ul>
+            <li><a href="#stack-list">list</a></li>
+            <li><a href="#stack-describe">describe</a></li>
+            <li><a href="#stack-create">create</a></li>
+        </ul>
+    </li>
+</ul>
