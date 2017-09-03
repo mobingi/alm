618f9156ddaaacc752fe7c008bebcbf234c32993
waylandzhang <waylandzhang@gmail.com>
2017-09-03 14:57:08 +0900

    update documentation

---
+++ docs/markdown/alm-template/doc-alm-template-sidebar.md.php
@@ -0,0 +1,54 @@
+<!-- Sidebar is not parsed thru script to give you more control over style-->
+
+<ul class="nav sidenav dropable sticky">
+
+    <li>
+        <a href="<?php echo $siteUrl; ?>what-is-alm-template">What is ALM Template</a>
+        <ul>
+            <li><a href="<?php echo $siteUrl; ?>what-is-alm-template#concepts">ALM Template Concepts</a></li>
+            <li><a href="<?php echo $siteUrl; ?>what-is-alm-template#how-does-it-work">How does ALM Template Work?</a></li>
+        </ul>
+    </li>
+    <li>
+        <a href="<?php echo $siteUrl; ?>alm-template-best-practices">Best Practices</a>
+        <ul>
+            <li><a href="<?php echo $siteUrl; ?>alm-template-best-practices#">sub_item</a></li>
+        </ul>
+    </li>
+    <li>
+        <a href="<?php echo $siteUrl; ?>working-with-alm-templates">Working with ALM Templates</a>
+        <ul>
+            <li><a href="<?php echo $siteUrl; ?>working-with-alm-templates#">sub_item</a></li>
+        </ul>
+    </li>
+    <li>
+        <a href="<?php echo $siteUrl; ?>alm-templates-reference">ALM Template Reference</a>
+        <ul>
+            <li><a href="<?php echo $siteUrl; ?>alm-templates-reference#">sub_item</a></li>
+        </ul>
+    </li>
+    <li>
+        <a href="<?php echo $siteUrl; ?>alm-templates-troubleshooting">Troubleshooting</a>
+        <ul>
+            <li><a href="<?php echo $siteUrl; ?>alm-templates-troubleshooting#">sub_item</a></li>
+        </ul>
+    </li>
+    <li>
+        <a href="<?php echo $siteUrl; ?>alm-templates-release-history">Release History</a>
+        <ul>
+            <li><a href="<?php echo $siteUrl; ?>alm-templates-release-history#">sub_item</a></li>
+        </ul>
+    </li>
+    <li>
+        <a href="<?php echo $siteUrl; ?>alm-templates-example-templates">Example ALM Templates</a>
+        <ul>
+            <li><a href="<?php echo $siteUrl; ?>alm-templates-example-templates#single-server">Single server</a></li>
+            <li><a href="<?php echo $siteUrl; ?>alm-templates-example-templates#single-server-with-hello-world">Single server with "Hello World"</a></li>
+            <li><a href="<?php echo $siteUrl; ?>alm-templates-example-templates#single-server-with-custom-subnet">Single server with custom Subnet</a></li>
+            <li><a href="<?php echo $siteUrl; ?>alm-templates-example-templates#single-server-with-custom-network-acl">Single server with custom Network ACL</a></li>
+            <li><a href="<?php echo $siteUrl; ?>alm-templates-example-templates#single-server-with-custom-security-group">Single server with custom Security Group</a></li>
+            <li><a href="<?php echo $siteUrl; ?>alm-templates-example-templates#load-balanced">Load-balanced server stack</a></li>
+        </ul>
+    </li>
+
+</ul>
